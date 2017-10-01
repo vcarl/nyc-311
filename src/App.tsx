a38db11c200c29a27151ff738092afa1cb49033b
Carl Vitullo <carl.vitullo@gmail.com>
2017-10-01 15:32:40 -0400

    Add sidebar with filter controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import * as React from "react";
 import request311, { CityRequest } from "./request311";
 import RequestMap from "./RequestMap";
+import Sidebar from "./Sidebar";
+import { FilterProvider, Filter } from "hepa";
 
 import "./App.css";
+import "./bootstrap.css";
 
 interface State {
-  requests: {
-    status: string;
-    error: string;
-    data: CityRequest[];
-  };
+  requests: FetchedData<CityRequest>;
+}
+
+interface FetchedData<T> {
+  status: string;
+  error: string;
+  data: T[];
 }
 
 class App extends React.Component<{}, State> {
@@ -22,7 +27,9 @@ class App extends React.Component<{}, State> {
   };
   componentDidMount() {
     this.setState({ requests: { status: "pending", error: "", data: [] } });
-
+    this.fetchRequests();
+  }
+  fetchRequests = () => {
     request311()
       .then(data =>
         this.setState({
@@ -36,16 +43,41 @@ class App extends React.Component<{}, State> {
       .catch(error =>
         this.setState({ requests: { status: "error", error, data: [] } })
       );
-  }
+  };
   render() {
-    switch (this.state.requests.status) {
-      case "error":
-        return <div>JSON.stringify(this.state.requests.error)</div>;
-      case "pending":
-        return <div>"loading..."</div>;
-      default:
-        return <RequestMap requests={this.state.requests.data} />;
-    }
+    type TypedFilterProvider = new () => FilterProvider<CityRequest>;
+    const TypedFilterProvider = FilterProvider as TypedFilterProvider;
+
+    return (
+      <TypedFilterProvider>
+        <div className="app-root">
+          {(function(requests: FetchedData<CityRequest>) {
+            switch (requests.status) {
+              case "error":
+                return <div>JSON.stringify(requests.error)</div>;
+              case "pending":
+                return <div className="loading-state">loading...</div>;
+              default:
+                type TypedFilter = new () => Filter<CityRequest>;
+                const TypedFilter = Filter as TypedFilter;
+                return (
+                  <TypedFilter
+                    style={{ width: "100%" }}
+                    data={requests.data}
+                    render={data => <RequestMap requests={data} />}
+                  />
+                );
+            }
+          })(this.state.requests)}
+          <div className="controls">
+            <Sidebar
+              requests={this.state.requests.data}
+              refresh={this.fetchRequests}
+            />
+          </div>
+        </div>
+      </TypedFilterProvider>
+    );
   }
 }
 
---
+++ src/Sidebar.tsx
@@ -0,0 +1,59 @@
+import * as React from "react";
+import { Exact } from "hepa";
+import { CityRequest } from "./request311";
+import { uniq } from "ramda";
+
+interface Props {
+  requests: CityRequest[];
+  refresh: React.EventHandler<React.MouseEvent<HTMLButtonElement>>;
+}
+
+const SelectFilter = ({
+  name,
+  children,
+  ...rest
+}: {
+  name: string;
+  children: (JSX.Element | JSX.Element[])[];
+  [key: string]: any;
+}) => {
+  type TypedExact = new () => Exact<CityRequest>;
+  const TypedExact = Exact as TypedExact;
+  return (
+    <TypedExact
+      name={name}
+      render={({
+        value,
+        onChange
+      }: {
+        value: string;
+        onChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
+      }) => (
+        <select value={value} onChange={onChange} {...rest}>
+          {children}
+        </select>
+      )}
+    />
+  );
+};
+
+const Sidebar = ({ requests, refresh }: Props) => (
+  <div>
+    <div className="form-group">
+      <label htmlFor="agency_name">Filter by Agency</label>
+      <SelectFilter className="form-control" name="agency_name">
+        <option value="">All</option>
+        {uniq(requests.map(r => r.agency_name)).map(name => (
+          <option value={name} key={name}>
+            {name}
+          </option>
+        ))}
+      </SelectFilter>
+    </div>
+    <button className="btn btn-sm btn-primary btn-block" onClick={refresh}>
+      Reload Data (data changes daily)
+    </button>
+  </div>
+);
+
+export default Sidebar;
