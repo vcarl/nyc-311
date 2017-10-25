54af6c4a5fc41c787c797f4247987c7ee72101df
Carl Vitullo <carl.vitullo@gmail.com>
2017-10-25 12:52:26 -0400

    Group data by different fields, track as keyed object

--- src/App.tsx
+++ src/App.tsx
@@ -4,30 +4,55 @@ import RequestMap from "./RequestMap";
 import Sidebar from "./Sidebar";
 import Attribution from "./Attribution";
 import { FilterProvider, Filter } from "hepa";
+import { groupBy } from "ramda";
 
 import "./App.css";
 import "./bootstrap.css";
 
 interface State {
-  requests: FetchedData<CityRequest>;
+  requests: FetchedData;
 }
 
-interface FetchedData<T> {
+type FetchedData = {
   status: string;
   error: string;
-  data: T[];
-}
+  data: DataMap;
+};
+
+type DataMap = {
+  keyed: { [key: string]: CityRequest };
+  byAgency: { [key: string]: string[] };
+  byBorough: { [key: string]: string[] };
+};
+
+const handleData = (data: CityRequest[]) => {
+  let requests = {} as DataMap;
+  let keyed = {};
+
+  data.forEach(x => (keyed[x.unique_key] = x));
+  requests.keyed = keyed;
+  requests.byAgency = groupBy(x => keyed[x].agency, Object.keys(keyed));
+  return requests;
+};
+
+const emptyData: DataMap = {
+  keyed: {},
+  byAgency: {},
+  byBorough: {}
+};
 
 class App extends React.Component<{}, State> {
   state = {
     requests: {
       status: "pending",
       error: "",
-      data: [] as CityRequest[]
+      data: emptyData
     }
   };
   componentDidMount() {
-    this.setState({ requests: { status: "pending", error: "", data: [] } });
+    this.setState({
+      requests: { status: "pending", error: "", data: emptyData }
+    });
     this.fetchRequests();
   }
   fetchRequests = () => {
@@ -37,12 +62,12 @@ class App extends React.Component<{}, State> {
           requests: {
             status: "idle",
             error: "",
-            data
+            data: handleData(data)
           }
         })
       )
       .catch(error =>
-        this.setState({ requests: { status: "error", error, data: [] } })
+        this.setState({ requests: { status: "error", error, data: emptyData } })
       );
   };
   render() {
@@ -52,7 +77,7 @@ class App extends React.Component<{}, State> {
     return (
       <TypedFilterProvider>
         <div className="app-root">
-          {(function(requests: FetchedData<CityRequest>) {
+          {(function(requests: FetchedData) {
             switch (requests.status) {
               case "error":
                 return <div>JSON.stringify(requests.error)</div>;
@@ -64,17 +89,19 @@ class App extends React.Component<{}, State> {
                 return (
                   <TypedFilter
                     style={{ width: "100%" }}
-                    data={requests.data}
+                    data={Object.values(requests.data.keyed)}
                     render={data => <RequestMap requests={data} />}
                   />
                 );
             }
           })(this.state.requests)}
           <div className="controls">
-            <Sidebar
-              requests={this.state.requests.data}
-              refresh={this.fetchRequests}
-            />
+            {
+              <Sidebar
+                requests={this.state.requests.data.keyed}
+                refresh={this.fetchRequests}
+              />
+            }
             <Attribution />
           </div>
         </div>
